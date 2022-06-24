9db07a741e4e758ed1260738b7518ca04f1c19c5
SummaryPuppet <adrianm_sv@hotmail.com>
2022-06-24 01:42:01 +0000

    add colors

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import './style.css'
 
+const button = document.getElementById("toggle")
 
+const useDark = window.matchMedia("(prefers-color-scheme: dark)")
+
+button.addEventListener("click", ()=>{
+
+})
