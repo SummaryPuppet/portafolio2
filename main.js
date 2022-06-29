49372ec2f30efa92b454e551e98826a4f4ea7a06
SummaryPuppet <adrianm_sv@hotmail.com>
2022-06-29 18:14:50 +0000

    add media queries

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
 import './style.css'
 
-const button = document.getElementById("toggle")
-
-const useDark = window.matchMedia("(prefers-color-scheme: dark)")
-
-button.addEventListener("click", ()=>{
-
-})
